8b4d18553914c7dad1fa2dadb494065927e2623d
Keranak <43937511+Keranak@users.noreply.github.com>
2023-06-12 12:16:47 +0000

    Ajout du logo

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -21,7 +21,7 @@ const Navbar: React.FC = () => {
   return (
     <nav>
       <div className="navbar-container">
-        <div className="logo">Logo</div>
+        <div className="logo"><img src="/LOGO_DELIRIUM" alt="logo de delirium" /></div>
         <ul className={`nav-links ${isMenuOpen ? 'active' : ''}`}>
           {navLinks.map((link, index) => (
             <li key={index}>
