2ac4764030c2559f6533a14063124a2fd67939cd
Keranak <43937511+Keranak@users.noreply.github.com>
2023-06-13 14:26:53 +0000

    refonte footer responsive

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,53 +1,53 @@
-import { FiHome, FiLogIn, FiUserPlus, FiMail, FiInfo } from 'react-icons/fi';
 import { FaInstagram, FaYoutube, FaTiktok, FaLinkedin } from 'react-icons/fa';
 import Image from 'next/image';
 
 const Footer = () => {
   return (
     <footer className="bg-gray-900 text-white py-8">
-      <div className="container mx-auto flex justify-center items-center">
+      <div className="container mx-auto flex flex-col sm:flex-row justify-center items-center">
         <Image
+          className="max-w-full"
           src="/LOGO_DELIRIUM.png"
           alt="logo"
           width={142.51}
           height={247}
         />
 
-        <nav className="ml-6">
-          <ul className="flex">
+        <nav className="ml-6 flex flex-col sm:flex-row">
+          <ul className="flex flex-col sm:flex-row">
             <li className="mr-4">
               <a href="/" className="flex items-center text-gray-300 hover:text-white">
-                <FiHome className="mr-2" />
+                
                 Accueil
               </a>
             </li>
             <li className="mr-4">
               <a href="/connexion" className="flex items-center text-gray-300 hover:text-white">
-                <FiLogIn className="mr-2" />
+                
                 Connexion
               </a>
             </li>
             <li className="mr-4">
               <a href="/inscription" className="flex items-center text-gray-300 hover:text-white">
-                <FiUserPlus className="mr-2" />
+                
                 Inscription
               </a>
             </li>
             <li className="mr-4">
               <a href="/contact" className="flex items-center text-gray-300 hover:text-white">
-                <FiMail className="mr-2" />
+                
                 Contact
               </a>
             </li>
             <li>
               <a href="/apropos" className="flex items-center text-gray-300 hover:text-white">
-                <FiInfo className="mr-2" />
+                
                 À propos de nous
               </a>
             </li>
           </ul>
         </nav>
-          <br />
+        <br />
         <div className="ml-6">
           <ul className="flex">
             <li className="mr-2">
