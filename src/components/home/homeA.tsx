775aac2b28a94ff29ce187e703a604bf3f32fe78
Keranak <43937511+Keranak@users.noreply.github.com>
2023-06-13 12:27:47 +0000

    other test css

--- src/components/home/homeA.tsx
+++ src/components/home/homeA.tsx
@@ -7,6 +7,7 @@ export default function HomeA() {
     return (
         <main>
         <Adrenaline />
+        <figure className="vague"></figure>
         <QuiSommesNous />
         <NosJeux />
         <Comments />
