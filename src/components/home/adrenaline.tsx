7bd8592e4e924f22866819389fbd84a38a9b30da
Keranak <43937511+Keranak@users.noreply.github.com>
2023-06-13 07:52:24 +0000

    test section 1

--- src/components/home/adrenaline.tsx
+++ src/components/home/adrenaline.tsx
@@ -6,10 +6,10 @@ function Adrenaline() {
     return (
         <section className="bgBlack">
         <div className="container">
+      <aside>
       <h3 className='h3 textWhite'>Explorez l'adrénaline, renforcez vos liens.</h3>
       <p className='textWhite'>Dépassez vos peurs et plongez dans nos palpitants survival
       game et testez vos limites !</p>
-      <aside>
         <Image src="/MASCOTTE_HEADER.png" alt="mascotte" width={175.35} height={197.73} />
       </aside>
       </div>   
