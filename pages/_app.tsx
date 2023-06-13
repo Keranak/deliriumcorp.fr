97b1ac91dfb46fd422644ce7f484c631031a39aa
Keranak <43937511+Keranak@users.noreply.github.com>
2023-06-13 09:35:35 +0000

    Add Matomo et GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
+
+import TagManager from 'gtm-for-react'
+import { env } from 'process'
 
 export default function App({ Component, pageProps }: AppProps) {
+
+useEffect(() => {
+  const tagManagerArgs:any = {
+    gtmId: env.GTM_MANAGER_ID,
+}
+
+  TagManager.initialize(tagManagerArgs)
+}, [])
+
   return <Component {...pageProps} />
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,21 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 import Navbar from '@/src/components/navbar'
 import Footer from '@/src/components/footer'
+import React from 'react'
+
+
+
+
 
 export default function Document() {
+
   return (
     <Html lang="fr">
       <Head>
         <title>Delirium-Corp</title>
+        
+        
+
         
       </Head>
       <body>
