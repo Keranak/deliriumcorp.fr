202312305544824e496882896263b246ef0df55d
Keranak <43937511+Keranak@users.noreply.github.com>
2023-06-13 08:15:55 +0000

    adrenaline changement et ajout css

--- src/components/home/adrenaline.tsx
+++ src/components/home/adrenaline.tsx
@@ -6,13 +6,17 @@ function Adrenaline() {
     return (
         <section className="bgBlack">
         <div className="container">
-      <aside>
-      <h3 className='h3 textWhite'>Explorez l'adrénaline, renforcez vos liens.</h3>
-      <p className='textWhite'>Dépassez vos peurs et plongez dans nos palpitants survival
-      game et testez vos limites !</p>
-        <Image src="/MASCOTTE_HEADER.png" alt="mascotte" width={175.35} height={197.73} />
-      </aside>
-      </div>   
+          <aside>
+            <div className="text-container">
+              <h3 className="h3 textWhite">Explorez l'adrénaline, renforcez vos liens.</h3>
+              <p className="textWhite">Dépassez vos peurs et plongez dans nos palpitants survival game et testez vos limites !</p>
+            </div>
+            <div className="image-container">
+              <Image src="/MASCOTTE_HEADER.png" alt="mascotte" width={175.35} height={197.73} />
+            </div>
+          </aside>
+        </div>
+
       </section>
     )
 }
