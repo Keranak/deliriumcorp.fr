6952cbda3f8ca0e5b0601e22f3217eeb491d2fe7
Keranak <43937511+Keranak@users.noreply.github.com>
2023-06-13 07:48:14 +0000

    correction bug matomo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,6 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 import Navbar from '@/src/components/navbar'
 import Footer from '@/src/components/footer'
-import Matomo from '@/src/components/matomo'
 
 export default function Document() {
   return (
