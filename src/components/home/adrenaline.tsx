ee0568465a9325a347045a2f57bcad26e06ccac2
Keranak <43937511+Keranak@users.noreply.github.com>
2023-06-13 08:19:22 +0000

    correction css et renommage des classes

--- src/components/home/adrenaline.tsx
+++ src/components/home/adrenaline.tsx
@@ -7,11 +7,11 @@ function Adrenaline() {
         <section className="bgBlack">
         <div className="container">
           <aside>
-            <div className="text-container">
-              <h3 className="h3 textWhite">Explorez l'adrénaline, renforcez vos liens.</h3>
+            <div className="text-container text-adrenaline">
+              <h3 className="h2 textWhite">Explorez l'adrénaline, renforcez vos liens.</h3>
               <p className="textWhite">Dépassez vos peurs et plongez dans nos palpitants survival game et testez vos limites !</p>
             </div>
-            <div className="image-container">
+            <div className="adrenaline-image-container">
               <Image src="/MASCOTTE_HEADER.png" alt="mascotte" width={175.35} height={197.73} />
             </div>
           </aside>
