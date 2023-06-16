21aeef2b286488e9d10d1c6bed24e97799f9118a
Keranak <43937511+Keranak@users.noreply.github.com>
2023-06-16 16:24:58 +0000

    correction

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,71 +4,72 @@ import Image from 'next/image';
 const Footer = () => {
   return (
     <footer className="waveTop">
-      <section id='bottom'>
-
-      
-      <div className="container logoFooter">
-        <Image
-          className="max-w-full"
-          src="/LOGO_DELIRIUM.png"
-          alt="logo"
-          width={142}
-          height={247}/>
+      <section id='bottom' className="flex justify-center items-center flex-col">
+        <div className="container logoFooter">
+          <Image
+            className="max-w-full"
+            src="/LOGO_DELIRIUM.png"
+            alt="logo"
+            width={142}
+            height={247}
+          />
+        </div>
+        <div className='grid grid-cols-1 md:grid-cols-2 w-auto p-4'>
+          <div className='w-full'>
+            <ul id="links" className="text-lg">
+              <li>
+                <a href="#top" title='Accueil'>Accueil</a>
+              </li>
+              <li>
+                <a href="/apropos" title='À propos'>À propos</a>
+              </li>
+              <li>
+                <a href="/contact" title='Contact'>Contact</a>
+              </li>
+              <li>
+                <a href="/politics" title='Politique de confidentialité'>Politique de confidentialité</a>
+              </li>
+              <li>
+                <a href="/mentions-legales" title='Mentions Légales'>Mentions Légales</a>
+              </li>
+            </ul>
           </div>
-          <section className='leftDesktop'>
-          <ul id="links">
-            <li>
-              <a href="#top" title='Accueil'>Accueil</a>
-            </li>
-            <li>
-              <a href="/apropos" title='À propos'>À propos</a>
-            </li>
-            <li>
-              <a href="/contact" title='Contact'>Contact</a>
+          <div>
+            <ul className="text-lg">
+              <li>
+                <a href="/signin">Connexion</a>
               </li>
               <li>
-              <a href="/politics" title='Politique de confidentialité'>Politique de confidentialité</a>
+                <a href="/signup">Inscription</a>
               </li>
-              <li>
-              <a href="/mentions-legales" title='Mentions Légales'>Mentions Légales</a>
-              </li>
-          </ul>
-          </section>
-          <section  className='rightDesktop'>
+            </ul>
+            <ul id="socials" className="horizontal-icons flex justify-center text-3xl">
           <li>
-              <a href="/signin">Connexion</a>
-              </li>
-            <li>
-              <a href="/signup">Inscription</a>
-              </li>
-          
-
-          <ul id="socials" className="horizontal-icons">
-  <li>
-    <a href="https://www.instagram.com/delirium.corp/" target="_blank" rel="noopener noreferrer" className="text-gray-300 hover:text-white">
-      <FaInstagram />
-    </a>
-  </li>
-  <li>
-    <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer" className="text-gray-300 hover:text-white">
-      <FaYoutube />
-    </a>
-  </li>
-  <li>
-    <a href="https://www.facebook.com/profile.php?id=100093210124644" target="_blank" rel="noopener noreferrer" className="text-gray-300 hover:text-white">
-      <FaFacebook />
-    </a>
-  </li>
-  <li>
-    <a href="https://www.linkedin.com/company/d%C3%A9lirium-corp/" target="_blank" rel="noopener noreferrer" className="text-gray-300 hover:text-white">
-      <FaLinkedin />
-    </a>
-  </li>
-</ul>
-</section>
-</section>
-
-        </footer>
+            <a href="https://www.instagram.com/delirium.corp/" target="_blank" rel="noopener noreferrer" className="text-gray-300 hover:text-white">
+              <FaInstagram />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer" className="text-gray-300 hover:text-white">
+              <FaYoutube />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.facebook.com/profile.php?id=100093210124644" target="_blank" rel="noopener noreferrer" className="text-gray-300 hover:text-white">
+              <FaFacebook />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/company/d%C3%A9lirium-corp/" target="_blank" rel="noopener noreferrer" className="text-gray-300 hover:text-white">
+              <FaLinkedin />
+            </a>
+          </li>
+        </ul>
+          </div>
+        </div>
+       
+      </section>
+    </footer>
   );
 };
 
--- src/components/slider.tsx
+++ src/components/slider.tsx
@@ -1,23 +1,64 @@
-import useEmblaCarousel, { EmblaOptionsType } from "embla-carousel-react";
-import { PropsWithChildren } from "react";
-
-// 1. define the props
-type Props = { options?: EmblaOptionsType } & PropsWithChildren;
-
-const Slider = ({ children, options }: Props) => {
-  // 2. initialize EmblaCarousel using the custom hook
-  const [emblaRef] = useEmblaCarousel({
-    slidesToScroll: 1,
-    align: "start",
-    ...options,
+import { useEffect, useRef } from "react";
+import Image from "next/image";
+import useEmblaCarousel from "embla-carousel-react"; 
+const Carousel = () => {
+  const carouselRef = useRef(null);
+  const [emblaRef, emblaApi] = useEmblaCarousel({
+    loop: true,
   });
 
+  useEffect(() => {
+    if (emblaApi) {
+      emblaApi.on("init", () => {
+        // Faire quelque chose lors de l'initialisation
+      });
+
+      emblaApi.on("select", () => {
+        // Faire quelque chose lorsqu'une diapositive est sélectionnée
+      });
+    }
+  }, [emblaApi]);
+
   return (
-    // 3. set ref as emblaRef.
-    // make sure we have overflow-hidden and flex so that it displays properly
-    <div className="overflow-hidden" ref={emblaRef}>
-      <div className="flex gap-10">{children}</div>
+    <div className="embla" ref={carouselRef}>
+      <div className="embla__viewport" ref={emblaRef}>
+        <div className="embla__container">
+          {/* Slides here */}
+          <div className="embla__slide">
+            <div className="slide-content bg-white p-8 rounded-md">
+              <h2 className="text-xl font-bold mb-4">John Doe</h2>
+              <p className="text-gray-800">Commentaire de John Doe...</p>
+            </div>
+          </div>
+          <div className="embla__slide">
+            <div className="slide-content bg-white p-8 rounded-md">
+              <h2 className="text-xl font-bold mb-4">Jane Smith</h2>
+              <p className="text-gray-800">Commentaire de Jane Smith...</p>
+            </div>
+          </div>
+          <div className="embla__slide">
+            <div className="slide-content bg-white p-8 rounded-md">
+              <h2 className="text-xl font-bold mb-4">Alice Johnson</h2>
+              <p className="text-gray-800">Commentaire d'Alice Johnson...</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="embla__dots flex justify-center mt-4"></div>
+      <button
+        className="embla__prev absolute left-4 top-1/2 transform -translate-y-1/2 bg-transparent border-none focus:outline-none"
+        onClick={() => emblaApi && emblaApi.scrollPrev()}
+      >
+        <Image width={50} height={50} src="/flechetemoin.png" alt="fleche gauche" />
+      </button>
+      <button
+        className="embla__next absolute right-4 top-1/2 transform -translate-y-1/2 bg-transparent border-none focus:outline-none"
+        onClick={() => emblaApi && emblaApi.scrollNext()}
+      >
+        <Image src="/flechetemoind.png" alt="fleche droite" width={50} height={50} />
+      </button>
     </div>
   );
 };
-export default Slider;
+
+export default Carousel;
