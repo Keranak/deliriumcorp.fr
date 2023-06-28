09075257bd162b67f4f051168cbeec89c373292f
Keranak <43937511+Keranak@users.noreply.github.com>
2023-06-28 18:04:40 +0000

    aujustement et update vidéo

--- src/components/home/adrenaline.tsx
+++ src/components/home/adrenaline.tsx
@@ -3,9 +3,9 @@ import React from "react";
 
 function Adrenaline() {
     return (
-        <article id="Adrenaline" className=" ">
-          <h1 className="AdrenalineTitle">Explorez l'adrénaline, renforcez vos liens.</h1>
-        <p className="AdrenalineTexte">Dépassez vos peurs et plongez dans nos palpitants survival
+        <article id="Adrenaline" className=" text-left">
+          <h1 className="AdrenalineTitle text-left">Explorez l'adrénaline, renforcez vos liens.</h1>
+        <p className="AdrenalineTexte text-left">Dépassez vos peurs et plongez dans nos palpitants survival
           game et testez vos limites !</p>
         </article>
       
