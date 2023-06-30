ec8fcea2f80a511a2b56d20f847ae6b781fc8bf6
Keranak <43937511+Keranak@users.noreply.github.com>
2023-06-30 10:08:08 +0000

    add legal mentions

---
+++ src/pages/mentions-legales.tsx
@@ -0,0 +1,48 @@
+import Head from "next/head";
+import Header from "../components/header";
+
+
+export default function mentionlegales(){
+    return <>
+        <Head>Mentions Légales - DeliriumCorp</Head>
+    <Header/>
+
+    <section className="container">
+        
+Bien sûr ! Voici la version HTML des mentions légales :
+
+  <h1>Mentions légales</h1>
+
+  <h2>1. Informations générales</h2>
+  <p>Le présent site web est exploité par [Nom de l'entreprise] (ci-après dénommé "nous", "notre" ou "l'entreprise"), une société immatriculée [Numéro d'immatriculation] et dont le siège social est situé à [Adresse du siège social]. Nous sommes soumis aux lois en vigueur dans [Pays].</p>
+
+  <h2>2. Propriété intellectuelle</h2>
+  <p>Tous les contenus présents sur ce site, y compris, mais sans s'y limiter, les textes, les graphiques, les images, les logos, les icônes, les clips audio, les téléchargements numériques et les compilations de données, sont la propriété exclusive de l'entreprise ou de ses fournisseurs de contenu, et sont protégés par les lois relatives à la propriété intellectuelle.</p>
+  <p>Il est strictement interdit de reproduire, distribuer, modifier, afficher, transmettre ou utiliser de quelque manière que ce soit le contenu de ce site sans l'autorisation préalable et écrite de l'entreprise.</p>
+
+  <h2>3. Collecte de données personnelles</h2>
+  <p>Nous attachons une grande importance à la protection de votre vie privée. Nous collectons et utilisons vos données personnelles conformément à notre <a href="[Lien vers la politique de confidentialité]">politique de confidentialité</a>. En utilisant ce site, vous consentez à la collecte, à l'utilisation et au traitement de vos données personnelles conformément à ladite politique de confidentialité.</p>
+
+  <h2>4. Utilisation des cookies</h2>
+  <p>Ce site utilise des cookies pour améliorer votre expérience de navigation. Les cookies sont de petits fichiers texte stockés sur votre ordinateur par votre navigateur web. Ils permettent de se souvenir de vos préférences, de personnaliser votre expérience sur le site et d'analyser la manière dont vous interagissez avec celui-ci.</p>
+  <p>En utilisant ce site, vous consentez à l'utilisation des cookies conformément à notre <a href="[Lien vers la politique en matière de cookies]">politique en matière de cookies</a>.</p>
+
+  <h2>5. Liens vers des sites tiers</h2>
+  <p>Ce site peut contenir des liens vers des sites web de tiers. Ces liens sont fournis uniquement pour votre commodité. L'entreprise n'exerce aucun contrôle sur ces sites tiers et n'est pas responsable de leur contenu ou de leurs pratiques en matière de confidentialité. L'accès à ces sites se fait à vos propres risques.</p>
+
+  <h2>6. Limitation de responsabilité</h2>
+  <p>L'entreprise s'efforce de fournir des informations précises et à jour sur ce site, mais ne peut garantir leur exhaustivité, leur exactitude ou leur actualité. L'utilisation de ce site se fait à vos propres risques. L'entreprise décline toute responsabilité quant aux dommages directs, indirects, spéciaux, consécutifs ou punitifs résultant de l'utilisation de ce site ou de l'incapacité à l'utiliser.</p>
+
+  <h2>7. Modifications des mentions légales</h2>
+  <p>L'entreprise se réserve le droit de modifier les présentes mentions légales à tout moment et sans préavis. Les modifications seront effectives dès leur publication sur ce site. Il est de votre responsabilité de consulter régulièrement ces mentions légales pour prendre connaissance des éventuelles modifications.</p>
+
+  <h2>8. Contact</h2>
+  <p>Si vous avez des questions concernant ces mentions légales, veuillez nous contacter à l'adresse suivante :</p>
+  <p>contactsaphiragency@gmail.com</p>
+
+  <p>Dernière mise à jour : [Date de la dernière mise à jour]</p>
+
+    </section>
+    </>
+
+}
---
+++ src/pages/politics.tsx
@@ -0,0 +1,56 @@
+import Head from "next/head";
+import Header from "../components/header";
+
+export default function politics(){
+    return <>
+            <Head>Politique de confidentialité - DeliriumCorp</Head>
+    <Header/>
+    <h1>Politique de confidentialité</h1>
+
+<h2>Collecte de données personnelles</h2>
+<p>Lorsque vous utilisez notre site web, nous pouvons collecter les données personnelles suivantes :</p>
+<ul>
+  <li>Votre nom, votre adresse e-mail et d'autres informations de contact que vous nous fournissez lorsque vous vous inscrivez à notre newsletter ou lorsque vous nous contactez via le formulaire de contact.</li>
+  <li>Les informations que vous fournissez lorsque vous effectuez un achat ou une commande sur notre site web, telles que votre nom, votre adresse de livraison et vos informations de paiement.</li>
+  <li>Les données de navigation, y compris votre adresse IP, le type de navigateur que vous utilisez, les pages que vous visitez sur notre site web et d'autres données de diagnostic.</li>
+</ul>
+
+<h2>Utilisation des données personnelles</h2>
+<p>Nous utilisons les données personnelles que nous collectons pour les finalités suivantes :</p>
+<ul>
+  <li>Fournir, exploiter et maintenir notre site web.</li>
+  <li>Personnaliser et améliorer votre expérience sur notre site web.</li>
+  <li>Répondre à vos demandes, commentaires et questions.</li>
+  <li>Vous envoyer des informations promotionnelles, des newsletters et d'autres communications relatives à notre entreprise, sous réserve de votre consentement préalable.</li>
+  <li>Traiter vos commandes et gérer les transactions que vous effectuez sur notre site web.</li>
+  <li>Détecter, prévenir et résoudre les problèmes techniques ou de sécurité.</li>
+</ul>
+
+<h2>Partage des données personnelles</h2>
+<p>Nous pouvons partager vos données personnelles avec les tiers suivants :</p>
+<ul>
+  <li>Nos fournisseurs de services qui nous aident à exploiter notre site web et à fournir nos services.</li>
+  <li>Les autorités gouvernementales, les organismes de réglementation et les organismes d'application de la loi, lorsque cela est nécessaire pour se conformer à une obligation légale ou pour protéger nos droits légaux.</li>
+</ul>
+
+<h2>Cookies</h2>
+<p>Nous utilisons des cookies et des technologies similaires sur notre site web pour collecter et suivre des informations sur votre utilisation du site. Pour plus d'informations sur l'utilisation des cookies, veuillez consulter notre politique en matière de cookies.</p>
+
+<h2>Conservation des données</h2>
+<p>Nous conservons vos données personnelles aussi longtemps que nécessaire pour atteindre les finalités décrites dans cette politique de confidentialité, sauf si une période de conservation plus longue est requise ou autorisée par la loi.</p>
+
+<h2>Sécurité des données</h2>
+<p>Nous mettons en place des mesures de sécurité appropriées pour protéger vos données personnelles contre tout accès non autorisé, toute divulgation, toute altération ou toute destruction.</p>
+
+<h2>Vos droits</h2>
+<p>Vous avez certains droits concernant vos données personnelles, notamment le droit d'accéder à vos données, de les rectifier, de les effacer et de limiter leur traitement. Veuillez nous contacter pour exercer ces droits ou pour toute demande de renseignements supplémentaires.</p>
+
+<h2>Modifications de la politique de confidentialité</h2>
+<p>Nous nous réservons le droit de modifier cette politique de confidentialité à tout moment. La version mise à jour sera publiée sur notre site web avec la date de la dernière mise à jour.</p>
+
+
+
+<p>Dernière mise à jour : [Date de la dernière mise à jour]</p>
+
+    </>
+}
